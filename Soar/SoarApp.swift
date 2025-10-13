31489ba45f06539106cd9aa3c08473fb3a84ef62
kartikayy007 <kartikayy6969@gmail.com>
2025-10-13 21:57:04 +0530

    Initial Commit

---
+++ Soar/SoarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoarApp.swift
+//  Soar
+//
+//  Created by kartikay on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SoarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
